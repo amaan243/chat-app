ae2310999e2703369eda9e3f5c7888c9ff1b2a98
Amaan Patel <pa8131895@gmail.com>
2025-12-09 08:18:31 -0700

    server.js updated for vercel

--- server/server.js
+++ server/server.js
@@ -97,10 +97,16 @@ app.use('/api/messages',messageRouter);
 
 await connectDB();
 
-const PORT = process.env.PORT || 3000;
 
+if(process.env.NODE_ENV !== 'production'){
+  const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+}
+
+//Export server for vercel
+export default server;
 
 
