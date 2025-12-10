fc071d9718f3758e4ca0e4892bbca45c96b26116
Amaan Patel <pa8131895@gmail.com>
2025-12-10 05:58:13 -0700

    commit-render

--- server/server.js
+++ server/server.js
@@ -98,15 +98,14 @@ app.use('/api/messages',messageRouter);
 await connectDB();
 
 
-if(process.env.NODE_ENV !== 'production'){
+
   const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-}
 
-//Export server for vercel
-export default server;
+
+
 
 
