819bb64aee146b5734ade0dd7409231823d57a5c
Amaan Patel <pa8131895@gmail.com>
2025-12-13 06:46:58 -0700

    final-menu_icon-change

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -18,6 +18,7 @@ const Sidebar = () => {
 
     const { logout, onlineUsers } = useContext(AuthContext);
     const [input, setInput] = useState("");
+    const [menuOpen, setMenuOpen] = useState(false);
     const navigate = useNavigate();
 
     // Filter user list based on search
@@ -31,6 +32,12 @@ const Sidebar = () => {
         getUsers();
     }, [onlineUsers]);
 
+    useEffect(() =>{
+        const close =()=>setMenuOpen(false);
+        document.addEventListener("click", close);
+        return () => document.removeEventListener("click", close);
+    }, [])
+
 
 
     return (
@@ -39,13 +46,19 @@ const Sidebar = () => {
             <div className='pb-5'>
                 <div className='flex justify-between items-center'>
                     <img src={assets.logo} alt='logo' className='max-w-40' />
-                    <div className='relative py-2 group'>
-                        <img src={assets.menu_icon} alt='menu' className='max-h-5 cursor-pointer' />
-                        <div className='absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block'>
-                            <p onClick={() => navigate('/profile')} className='cursor-pointer text-sm'>Edit Profile</p>
+                    <div className='relative py-2 '>
+                        <img onClick={(e)=>{e.stopPropagation();  setMenuOpen((prev) => !prev)}} src={assets.menu_icon} alt='menu' className='max-h-5 cursor-pointer' />
+                       {menuOpen && (
+                        <div className='absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-gray-100 '>
+                            <p onClick={() =>{setMenuOpen(false);
+                                  navigate('/profile');}}
+                                  className='cursor-pointer text-sm'>
+                                    Edit Profile
+                                    </p>
                             <hr className='my-2 border-t border-gray-500' />
-                            <p onClick={() => logout()} className='cursor-pointer text-sm'>Logout</p>
+                            <p onClick={() =>{setMenuOpen(false);  logout();}} className='cursor-pointer text-sm'>Logout</p>
                         </div>
+                       )} 
                     </div>
                 </div>
 
